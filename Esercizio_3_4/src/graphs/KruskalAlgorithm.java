16fb78f392999e0d8ad53b97f9ec20e3c2e5c1fa
Mattia Carlino <carlino.mattia@educ.di.unito.it>
2021-06-06 16:14:13 +0000

    Fix Kruskal file

--- Esercizio_3_4/src/graphs/KruskalAlgorithm.java
+++ Esercizio_3_4/src/graphs/KruskalAlgorithm.java
@@ -7,11 +7,13 @@ import unionfindset.UnionFindSetException;
 
 public class KruskalAlgorithm<V, E extends Comparable<E>> {
 	/*
-	 * It takes as input an undirected and connect graph, and returns a
-	 * minimumSpanningForest (graph). If the number of nodes of
-	 * minimumSpanningForest is equals to the number of nodes of the initial graph
-	 * all the nodes are already reachable.
-	 */
+  	* It takes as input an undirected graph and returns a minimumSpanningForest (graph).
+  	* If a graph is connected the output will be a single tree which will contains
+  	* number of edges = number of nodes - 1. If the graph is not connected the output
+  	* will be a forest of trees, with number of edges lower than number of nodes - 1.
+  	* If the number of nodes of minimumSpanningForest is equals to the number of nodes of
+  	* the initial graph all the nodes are already reachable.
+  	*/
 	public Graph<V, E> kruskal(Graph<V, E> gr) throws GraphException, UnionFindSetException {
 		Graph<V, E> minimumSpanningForest = new Graph<>("undirected");
 		UnionFindSet<V> grSet = new UnionFindSet<>();
